26f8b187eeb08040cc84281a6a13863d7ffbc79e
darkhorse0725 <darktop0112@gmail.com>
2023-05-31 01:08:29 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,175 @@
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+  const [outputImg, setOutputImg] = useState(null);
+  const [prompt, setPrompt] = useState("");
+  const [strength, setStrength] = useState(0.5);
+  const [steps, setSteps] = useState(20);
+  const [guidance, setGuidance] = useState(7.5);
+  const [seed, setSeed] = useState(42);
+  const [scheduler, setScheduler] = useState("dpmsolver++");
+  const handleProcess = async () => {
+    const reader = new FileReader();
+    let base64String = "";
+    reader.readAsDataURL(selectedImg);
+    reader.onload = async function () {
+      base64String = `${reader.result
+        .replace("data:", "")
+        .replace(/^.+,/, "")}`;
+      const mimeType = "image/png";
+      const tempImg = `${base64String}`;
+      setOutputImg(tempImg);
+      const url = "https://api.getimg.ai/v1/stable-diffusion/image-to-image";
+
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_GETIMG_APIKEY}`,
+      };
+
+      const data = {
+        model: "realistic-vision-v1-3",
+        prompt: prompt,
+        negative_prompt: "Disfigured, cartoon, blurry",
+        image: tempImg,
+        strength: strength,
+        steps: steps,
+        guidance: guidance,
+        seed: seed,
+        num_images: 4,
+        scheduler: scheduler,
+        output_format: "png",
+        base_resolution: {
+          width: 512,
+          height: 512,
+        },
+      };
+      try {
+        const result = await axios.post(url, data, {
+          headers: headers,
+        });
+        const _outputImg = `data:${mimeType};base64,${result.data.image}`;
+        setOutputImg(_outputImg);
+      } catch (err) {
+        console.log("err = ", err);
+      }
+    };
+  };
+
+  return (
+    <div>
+      <div className="app-container">
+        <div className="title">
+          <h1>Generate Avatar demo site</h1>
+        </div>
+        <div className="generate-container">
+          <div className="input-part">
+            {selectedImg && (
+              <div>
+                <img
+                  className="input-image"
+                  src={URL.createObjectURL(selectedImg)}
+                  alt="input-image"
+                />
+              </div>
+            )}
+            <input
+              type="file"
+              onChange={(e) => {
+                setSelectedImg(e.target.files[0]);
+              }}
+            />
+            <div className="input-field">
+              <label>Prompts: </label>
+              <input
+                className="form-control-input"
+                type="string"
+                value={prompt}
+                onChange={(e) => {
+                  setPrompt(e.target.value);
+                }}
+              />
+            </div>
+            <div className="input-field">
+              <label>Strength: </label>
+              <input
+                className="form-control-input"
+                type="number"
+                value={strength}
+                onChange={(e) => {
+                  setStrength(e.target.value);
+                }}
+              />
+            </div>
+            <div className="input-field">
+              <label>Seed: </label>
+              <input
+                className="form-control-input"
+                type="number"
+                value={seed}
+                onChange={(e) => {
+                  setSeed(e.target.value);
+                }}
+              />
+            </div>
+            <div className="input-field">
+              <label>Guidance: </label>
+              <input
+                className="form-control-input"
+                type="number"
+                value={guidance}
+                onChange={(e) => {
+                  setGuidance(e.target.value);
+                }}
+              />
+            </div>
+            <div className="input-field">
+              <label>Steps: </label>
+              <input
+                className="form-control-input"
+                type="number"
+                value={steps}
+                onChange={(e) => {
+                  setSteps(e.target.value);
+                }}
+              />
+            </div>
+            <div className="input-field">
+              <label>Scheduler: </label>
+              <select
+                className="form-control-input"
+                value={scheduler}
+                onChange={(e) => {
+                  setScheduler(e.target.value);
+                }}
+              >
+                <option value={`euler_a`}>Euler_A</option>
+                <option value={`euler`}>Euler</option>
+                <option value={`lms`}>LMS</option>
+                <option value={`ddim`}>DDIM</option>
+                <option value={`dpmsolver++`}>DPMSOLVER++</option>
+                <option value={`pndm`}>PNDM</option>
+              </select>
+            </div>
+            <button
+              style={{ margin: "20px" }}
+              className="form-control-input"
+              onClick={handleProcess}
+            >
+              Generate Avatar
+            </button>
+          </div>
+          <div className="output-part">
+            {outputImg && (
+              <img className="input-image" src={outputImg} alt="output-image" />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            34
+        </div>
+    )
+};
+
+export default Home;
